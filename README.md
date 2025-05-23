8d89a321d771d6571d9b19e3a2c37d197afd1863
Jay <jpca999@gmail.com>
2025-05-23 15:20:25 -0400

    WORKNIG FOR MONGO

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ when you command: - it shoudl ask you to login or register and then deploy.
 
 
 
+
+
+
--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const express = require('express');
+const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json());
+
+// MongoDB connection
+const uri = "mongodb+srv://jaylimousa:SBzVXE4LHqj16GEm@cluster0.1puycpp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
 
 // Define routes
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+  res.send("Hello from Express with MongoDB!");
 });
 
 app.get('/test', (req, res) => {
@@ -25,3 +38,5 @@ const server = awsServerlessExpress.createServer(app);
 exports.handler = (event, context) => {
   return awsServerlessExpress.proxy(server, event, context);
 };
+
+
