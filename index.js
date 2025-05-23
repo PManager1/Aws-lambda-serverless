1b5a0fe1a6a572cb54fd8d8dc6e3f6c349cffc63
Jay <jpca999@gmail.com>
2025-05-23 14:57:14 -0400

    WORKING LINKs, now testing other apis

---
+++ index.js
@@ -0,0 +1,23 @@
+const awsServerlessExpress = require('aws-serverless-express');
+const express = require('express');
+
+const app = express();
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+// For local dev
+if (process.env.NODE_ENV !== 'lambda') {
+  const PORT = process.env.PORT || 3005;
+  app.listen(PORT, () => {
+    console.log(`Express server running locally on http://localhost:${PORT}`);
+  });
+}
+
+// For Lambda
+const server = awsServerlessExpress.createServer(app);
+exports.handler = (event, context) => {
+  return awsServerlessExpress.proxy(server, event, context);
+};
