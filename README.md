56d904f81a735583652bc8648f0d3591db7ddba1
Jay <jpca999@gmail.com>
2025-05-23 15:20:58 -0400

    working after installing MONGO

--- README.md
+++ README.md
@@ -8,6 +8,3 @@ when you command: - it shoudl ask you to login or register and then deploy.
 
 
 
-
-
-
--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const express = require('express');
-const mongoose = require("mongoose");
 
 const app = express();
-app.use(express.json());
-
-// MongoDB connection
-const uri = "mongodb+srv://jaylimousa:SBzVXE4LHqj16GEm@cluster0.1puycpp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.error("MongoDB connection error:", err));
-
 
 // Define routes
 app.get('/', (req, res) => {
-  res.send("Hello from Express with MongoDB!");
+  res.send('Hello from Express!');
 });
 
 app.get('/test', (req, res) => {
@@ -38,5 +25,3 @@ const server = awsServerlessExpress.createServer(app);
 exports.handler = (event, context) => {
   return awsServerlessExpress.proxy(server, event, context);
 };
-
-
